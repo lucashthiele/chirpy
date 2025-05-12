5d01da21e87684645a64245fc8d18d64c5a7c814
lucashthiele <lucasthiele0@outlook.com>
2025-05-12 17:43:18 -0300

    routes

--- main.go
+++ main.go
@@ -1,17 +1,67 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port string = "42069"
 
+type apiConfig struct {
+	fileServerHits *atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetric() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		metricsResp := fmt.Sprintf(`<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`, cfg.fileServerHits.Load())
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(metricsResp))
+	})
+}
+
+func (cfg *apiConfig) handlerReset() http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Store(0)
+		w.WriteHeader(http.StatusOK)
+	})
+}
+
+func handleHealthz(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	res.WriteHeader(http.StatusOK)
+	res.Write([]byte("OK"))
+}
+
 func main() {
+	cfg := &apiConfig{
+		fileServerHits: &atomic.Int32{},
+	}
+	cfg.fileServerHits.Store(0)
 	mux := http.NewServeMux()
 	filepathRoot := http.Dir(".")
 
-	mux.Handle("/assets/", http.FileServer(filepathRoot))
+	handler := http.StripPrefix("/app/", http.FileServer(filepathRoot))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+
+	mux.HandleFunc("GET /api/healthz", handleHealthz)
+	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetric())
+	mux.HandleFunc("POST /admin/reset", cfg.handlerReset())
 
 	server := &http.Server{
 		Handler: mux,
