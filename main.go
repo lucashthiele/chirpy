48ba71dbc45440916443550e0c63935433f899eb
lucashthiele <lucasthiele0@outlook.com>
2025-05-13 16:15:36 -0300

    validate route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,18 @@ const port string = "42069"
 
 type apiConfig struct {
 	fileServerHits *atomic.Int32
+}
+
+type parameters struct {
+	Body string `json:"body"`
+}
+
+type errorResponse struct {
+	Error string `json:"error"`
+}
+
+type validResponse struct {
+	Valid bool `json:"valid"`
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -48,6 +61,55 @@ func handleHealthz(res http.ResponseWriter, req *http.Request) {
 	res.Write([]byte("OK"))
 }
 
+func handleValidateChirp(res http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		errResp := errorResponse{
+			Error: "Something went wrong",
+		}
+		resp, err := json.Marshal(errResp)
+		if err != nil {
+			log.Printf("Error enconding response: %s", err)
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		log.Printf("Error decoding parameters: %s", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		res.Write(resp)
+		return
+	}
+
+	if len(params.Body) > 140 {
+		res.WriteHeader(http.StatusBadRequest)
+
+		errResp := errorResponse{
+			Error: "Chirp is too long",
+		}
+		resp, err := json.Marshal(errResp)
+		if err != nil {
+			log.Printf("Error enconding response: %s", err)
+			res.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		res.Write(resp)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	validResp := validResponse{
+		Valid: true,
+	}
+	resp, err := json.Marshal(validResp)
+	if err != nil {
+		log.Printf("Error enconding response: %s", err)
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	res.Write(resp)
+}
+
 func main() {
 	cfg := &apiConfig{
 		fileServerHits: &atomic.Int32{},
@@ -59,9 +121,11 @@ func main() {
 	handler := http.StripPrefix("/app/", http.FileServer(filepathRoot))
 	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
 
-	mux.HandleFunc("GET /api/healthz", handleHealthz)
 	mux.HandleFunc("GET /admin/metrics", cfg.handlerMetric())
 	mux.HandleFunc("POST /admin/reset", cfg.handlerReset())
+
+	mux.HandleFunc("GET /api/healthz", handleHealthz)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
 
 	server := &http.Server{
 		Handler: mux,
